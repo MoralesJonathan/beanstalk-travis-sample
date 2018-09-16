f6761fddb1be92727ca1247eac84f3d51dc7b624
Adrian Rodriguez <adrianr0120@gmail.com>
2018-09-16 04:02:45 -0400

    review styles

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -3,13 +3,19 @@ import NavBar from './NavBar';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import axios from 'axios';
 
 const styles = {
     section: {
         display: "flex",
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        height:"100vh"
     },
     card: {
         minHeight:"400px",
@@ -18,12 +24,64 @@ const styles = {
     }
 }
 
-
+/*
+{
+    this.state.data.map((data, i) => {
+        if(i === 0) {
+            return (
+            <React.Fragment>
+            <Grid item lg={8} index={i}>
+                <Card style={styles.card}>
+                    <CardContent>
+                        <ul>
+                            <p>{data.text}</p>
+                        </ul>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item lg={4} index={i}>
+                <Card style={styles.card}>
+                    <CardContent>
+                        <h1>{data.date}</h1>
+                        <h2>{data.time}</h2>
+                        <h2>40%</h2>
+                    </CardContent>
+                </Card>
+            </Grid>
+            </React.Fragment>
+            )
+        }
+        return (
+            <Grid item lg={12} index={i}>
+                <Card style={styles.card}>
+                    <CardContent>
+                        <ul>
+                            <li>{data.date}</li>
+                            <li>{data.time}</li>
+                        </ul>
+                        <div style={{textAlign:"right"}}>
+                            <h1>20%</h1>
+                        </div>
+                    </CardContent>
+                </Card>
+            </Grid>
+        )
+    })
+}
+*/
 
 class Review extends Component {
     state = {
-        data:[]
+        data:[],
+        expanded: 'panel0'
     }
+
+    handleChange = panel => (event, expanded) => {
+      this.setState({
+        expanded: expanded ? panel : false,
+      });
+    };
+
     componentDidMount(){
         axios.get('https://api.myjson.com/bins/dh07k')
         .then(res => {
@@ -32,54 +90,41 @@ class Review extends Component {
     }
 
     render() {
+        const { expanded } = this.state;
+
         return (
             <React.Fragment>
                 <NavBar/>
                 <section style={styles.section}>
-                    <Grid container spacing={8}>
-                        {
-                            this.state.data.map((data, i) => {
-                                if(i === 0) {
-                                    return (
-                                    <React.Fragment>
-                                    <Grid item lg={8} index={i}>
-                                        <Card style={styles.card}>
-                                            <CardContent>
-                                                <ul>
-                                                    <p>{data.text}</p>
-                                                </ul>
-                                            </CardContent>
-                                        </Card>
-                                    </Grid>
-                                    <Grid item lg={4} index={i}>
-                                        <Card style={styles.card}>
-                                            <CardContent>
-                                                <h1>{data.date}</h1>
-                                                <h2>{data.time}</h2>
-                                                <h2>40%</h2>
-                                            </CardContent>
-                                        </Card>
-                                    </Grid>
-                                    </React.Fragment>
-                                    )
+                    <Grid container spacing={8} justify="center" alignItems="center">
+                        <Grid item lg={12} style={{marginTop:"15vh"}}>
+                            <div style={{marginLeft:"20vh", marginRight:"20vh"}}>
+                                {
+                                    this.state.data.map((data, i) => {
+                                        return (
+                                            <ExpansionPanel expanded={expanded === 'panel'+i} onChange={this.handleChange('panel'+i)}>
+                                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                                                    <Typography variant="title" style={{flexBasis: '20%'}}>{data.date}</Typography>
+                                                    <Typography>{data.time}</Typography>
+                                                </ExpansionPanelSummary>
+                                                <ExpansionPanelDetails>
+                                                    <Grid container justify="center" alignItems="center">
+                                                        <Grid item lg={8}>
+                                                            <Typography>
+                                                                {data.text}
+                                                            </Typography>
+                                                        </Grid>
+                                                        <Grid item lg={4}>
+                                                            <h1>40%</h1>
+                                                        </Grid>
+                                                    </Grid>
+                                                </ExpansionPanelDetails>
+                                            </ExpansionPanel>
+                                        )
+                                    })
                                 }
-                                return (
-                                    <Grid item lg={12} index={i}>
-                                        <Card style={styles.card}>
-                                            <CardContent>
-                                                <ul>
-                                                    <li>{data.date}</li>
-                                                    <li>{data.time}</li>
-                                                </ul>
-                                                <div style={{textAlign:"right"}}>
-                                                    <h1>20%</h1>
-                                                </div>
-                                            </CardContent>
-                                        </Card>
-                                    </Grid>
-                                )
-                            })
-                        }
+                            </div>
+                        </Grid>
                     </Grid>
                 </section>
             </React.Fragment>
