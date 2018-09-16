6605a76306e2cd2f07816110344a919cd9db02ee
Fulton Garcia <fultongarcia@gmail.com>
2018-09-16 08:03:27 -0400

    created cognitiveService

---
+++ middleware/cognitiveService.js
@@ -0,0 +1,23 @@
+var request = require('request');
+
+module.exports = (speech) =>{
+  var options = {
+    url: 'https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
+    headers: {
+      'Ocp-Apim-Subscription-Key' : process.env.MSFT_API_KEY
+    }
+  }
+  var documents = {
+    'documents' : [
+      {
+        'id': 1,
+        'language': 'en',
+        'text': speech
+      }
+    ]
+  }
+
+  request.post(options, documents).on('response', function(response) {
+    return response[0];
+  });
+}
--- routes/api/speech.js
+++ routes/api/speech.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const fillers = ['so', 'and', 'like', 'actually', 'you know', 'totally', 'i mean', 'just', 'literaly', 'so basically', 'anyway']; 
+const cognitiveService = require('../../middleware/cognitiveService');
+const fillers = ['so', 'and', 'like', 'actually', 'you know', 'totally', 'i mean', 'just', 'literaly', 'so basically', 'anyway'];
 const mongoClient = require('mongodb').MongoClient;
 const mongoDbUrl = `mongodb://root:password1@ds123790.mlab.com:23790/shellhacks`;
 
-router.get('/test',(req, res) => { 
-  res.json('the user endpoint works') 
+router.get('/test',(req, res) => {
+  res.json('the user endpoint works')
 })
 
 router.get('/frank/:collection/:timestamp', function (req, res) {
@@ -17,6 +18,7 @@ router.get('/frank/:collection/:timestamp', function (req, res) {
         }, function (err, speech) {
           if (err) res.send(false)
           speechString = speech.text.length > 1 ? speech.text.join(" ") : speech.text[0];
+          console.log(cognitiveService(speechString));
           let countMostUsedWord = new Promise(function (resolve, reject) {
             try {
               arr = speechString.split(" ")
