e5293e9e118a33f550b0ea384c76b6444a790424
Jonathan Morales <jonmorazav@Yahoo.com>
2018-09-18 13:44:18 -0400

    Beanstalk fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.port || 5001;
+const PORT = process.env.PORT || 8081;
 
 
 // MiddleWares
@@ -13,15 +13,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(mongoSanitize({ replaceWith: '_' }));
 
-// DB Config
-const db = `mongodb://${process.env.MONGOUSERNAME}:${process.env.MONGOPASSWORD}@ds123790.mlab.com:23790/shellhacks`;
 app.use(express.static('public'))
 
 // Connect to MongoDB
-mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+if(process.env.MONGOUSERNAME && process.env.MONGOPASSWORD) {
+    const db = `mongodb://${process.env.MONGOUSERNAME}:${process.env.MONGOPASSWORD}@ds123790.mlab.com:23790/shellhacks`;
+    mongoose
+        .connect(db)
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => console.log(err));
+}
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -33,5 +34,5 @@ app.use(routes);
 
 // Listen on port
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on da port ${PORT}`)
 });
